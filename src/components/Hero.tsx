672902b9f6b33380607bae2ef9119e566a94f32c
agustinsadaca <agustinsadaca@gmail.com>
2025-05-29 23:29:38 -0300

    logo change, animation added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,11 @@
 'use client'
-
 import styles from '@/styles/Hero.module.css'
+import Image from 'next/image'
 
 export default function Hero() {
   return (
     <section className={styles.hero}>
-      <img src="/logo.svg" alt="logo" className={styles.image} />
+      <Image src="/logo.svg" alt="logo" className={styles.image} width={180} height={120} />
       <button className={`${styles.button} fontUnageoRegularSemiBold fs24`} type="button">
         OFICINA TÉCNICA
       </button>
