8538d04be817bbc37a63f65d809f8988bf1f7cd5
agustinsadaca <agustinsadaca@gmail.com>
2026-03-16 00:09:53 -0300

    work on fix for new next version and language

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 const createNextIntlPlugin = require("next-intl/plugin");
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  turbopack: {
+    resolveAlias: {
+      'next-intl/config': './src/i18n/request.ts',
+    },
+  },
   images: {
     remotePatterns: [],
   },
